afdf38e0a82e537621f788c83c278b8cbf715c7e
stmacebal <mateo.cebquerol@gmail.com>
2022-12-24 08:16:08 +0100

    Adding EAA_Lab2\clock_type.h on 2022-12-24

---
+++ EAA_Lab2/clock_type.h
@@ -0,0 +1,25 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include "global.h"
+#include "statemachine.h" 
+
+//Container Object
+extern STATE_t CLOCK_fsm; 
+
+//Hour Object
+extern STATE_t CLOCK_hourWidget;
+
+//Minute Object
+extern STATE_t CLOCK_minuteWidget;
+
+/* [] END OF FILE */
